26e1b3b2215a95da413bc5ded127b2561b2cabf4
Quent-in <pages_quentin@hotmail.com>
2016-04-21 12:18:50 +0200

    French version
    I couldnt find the string for "yes!" and "no!", they may not be externalised yet. Let me know

---
+++ translations/harbour-chance-fr.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>Chance.</source>
+        <translation>Chance.</translation>
+    </message>
+    <message>
+        <source>This program answers you yes/no if you can`t make a decision. ;)</source>
+        <translation>Ce programme vous répond oui/non si vous ne pouvez pas vous décider. ;)</translation>
+    </message>
+    <message>
+        <source>License: GPLv3</source>
+        <translationLicense : GPLv3</translation>
+    </message>
+    <message>
+        <source>You can find the source code at the:</source>
+        <translation>Vous pouvez récupérer le code source ici : </translation>
+    </message>
+    <message>
+        <source>GitHub</source>
+        <translation>GitHub</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Chance.</source>
+        <translation>Chance.</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <source>Chance.</source>
+        <translation>Chance.</translation>
+    </message>
+    <message>
+        <source>Randomize!</source>
+        <translation>Au hasard !</translation>
+    </message>
+</context>
+</TS>
