fd1f889e03986bb8c88fcf5285df69609e938e12
Quent-in <pages_quentin@hotmail.com>
2016-08-03 19:43:52 +0200

    French local update

--- translations/harbour-chance-fr.ts
+++ translations/harbour-chance-fr.ts
@@ -25,19 +25,19 @@
     </message>
     <message>
         <source>Special thanks:</source>
-        <translation type="unfinished"></translation>
+        <translation>Remerciements : </translation>
     </message>
     <message>
         <source>-Quent-in for french translation</source>
-        <translation type="unfinished"></translation>
+        <translation>-Quent-in pour la version française</translation>
     </message>
     <message>
         <source>-amilcarsantos for code of shaking</source>
-        <translation type="unfinished"></translation>
+        <translation>-amilcarsantos pour le code des secousses</translation>
     </message>
     <message>
         <source>-Stefano (aka palikao) for italian translation and cool emojis</source>
-        <translation type="unfinished"></translation>
+        <translation>-Stefano (aka palikao) pour la version italienne et les emojis simpas</translation>
     </message>
 </context>
 <context>
@@ -48,11 +48,11 @@
     </message>
     <message>
         <source>Yes!</source>
-        <translation type="unfinished"></translation>
+        <translation>Oui !</translation>
     </message>
     <message>
         <source>No!</source>
-        <translation type="unfinished"></translation>
+        <translation>Non !</translation>
     </message>
 </context>
 <context>
@@ -67,87 +67,87 @@
     </message>
     <message>
         <source>Click or shake!</source>
-        <translation>Cliquez ou secouer!</translation>
+        <translation>Cliquez ou secouez !</translation>
     </message>
     <message>
         <source>It is certain.</source>
-        <translation type="unfinished"></translation>
+        <translation>C'est sûr.</translation>
     </message>
     <message>
         <source>It is decidedly so.</source>
-        <translation type="unfinished"></translation>
+        <translation>C'est résolument ainsi.</translation>
     </message>
     <message>
         <source>Without a doubt.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sans aucun doute.</translation>
     </message>
     <message>
         <source>Yes — definitely.</source>
-        <translation type="unfinished"></translation>
+        <translation>Oui - sans aucun doute.</translation>
     </message>
     <message>
         <source>You may rely on it.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vous pouvez y compter</translation>
     </message>
     <message>
         <source>As I see it, yes.</source>
-        <translation type="unfinished"></translation>
+        <translation>Comme je le vois, oui.</translation>
     </message>
     <message>
         <source>Most likely.</source>
-        <translation type="unfinished"></translation>
+        <translation>Il y a des chances</translation>
     </message>
     <message>
         <source>Outlook good.</source>
-        <translation type="unfinished"></translation>
+        <translation>Les perspectives sont bonnes.</translation>
     </message>
     <message>
         <source>Signs point to yes.</source>
-        <translation type="unfinished"></translation>
+        <translation>Les signes indiquent un oui.</translation>
     </message>
     <message>
         <source>Yes.</source>
-        <translation type="unfinished"></translation>
+        <translation>Oui.</translation>
     </message>
     <message>
         <source>Reply hazy, try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Réponse floue, réessayez plus tard.</translation>
     </message>
     <message>
         <source>Ask again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Redemandez plus tard.</translation>
     </message>
     <message>
         <source>Better not tell you now.</source>
-        <translation type="unfinished"></translation>
+        <translation>Il vaut mieux que nous ne sachiez pas pour le moment.</translation>
     </message>
     <message>
         <source>Cannot predict now.</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossible à dire pour l'instant.</translation>
     </message>
     <message>
         <source>Concentrate and ask again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Concentrez-vous et redemandez.</translation>
     </message>
     <message>
         <source>Don’t count on it.</source>
-        <translation type="unfinished"></translation>
+        <translation>N'y comptez pas.</translation>
     </message>
     <message>
         <source>My reply is no.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ma réponse est non.</translation>
     </message>
     <message>
         <source>My sources say no.</source>
-        <translation type="unfinished"></translation>
+        <translation>Mes sources disent que oui.</translation>
     </message>
     <message>
         <source>Outlook not so good.</source>
-        <translation type="unfinished"></translation>
+        <translation>Les perspectives ne sont pas bonnes.</translation>
     </message>
     <message>
         <source>Very doubtful.</source>
-        <translation type="unfinished"></translation>
+        <translation>Rien n'est moins sûr.</translation>
     </message>
 </context>
 </TS>
